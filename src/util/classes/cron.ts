ac6d13d08342771a8d3ad497dea235651e5f2f75
Margarita <Margarita>
2023-08-16 10:11:16 +0300

    refactor: add console.log

--- src/util/classes/cron.ts
+++ src/util/classes/cron.ts
@@ -6,10 +6,13 @@ export class Cron<T> {
   constructor(private callback: Function) {}
 
   start(time: string, ...args: T[]) {
-		console.log('cron start')
+		console.log('cron start', time)
     time = time.split(':').reverse().join(' ');
     this.cronId = cron.schedule(`${time} * * *`, async () =>
-      this.callback(...args),
+      {
+				console.log('shedule');
+				this.callback(...args)
+			}
     );
   }
 
