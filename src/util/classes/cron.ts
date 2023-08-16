8906ce9ea3d6adf2fcf16af51a83ce24285b2883
Margarita <Margarita>
2023-08-16 10:23:24 +0300

    refactor: delete test cron

--- src/util/classes/cron.ts
+++ src/util/classes/cron.ts
@@ -6,11 +6,9 @@ export class Cron<T> {
   constructor(private callback: Function) {}
 
   start(time: string, ...args: T[]) {
-		console.log('cron start', time)
     time = time.split(':').reverse().join(' ');
-		cron.schedule("*/10 * * * * *", function() {
-      console.log("running a task every 10 second");
-    });
+		console.log('cron start', time)
+
     this.cronId = cron.schedule(`${time} * * *`, async () =>
       {
 				console.log('shedule');
